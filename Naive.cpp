9adc1067ab8959c7f4a924fb0163e62d51faafa2
AbdoSoliman1 <pyramidfourth40@gmail.com>
2025-12-26 23:55:15 +0200

    Add runtime

--- Naive.cpp
+++ Naive.cpp
@@ -3,23 +3,23 @@ public:
 //Naive
     int maxSatisfaction(vector<int>& satisfaction)
     {
-        sort(satisfaction.begin(), satisfaction.end());
+        sort(satisfaction.begin(), satisfaction.end()); //→ O(n log n)
         int n = satisfaction.size();
         int maxSat = 0;
 
         // try all possible starting points
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++) //→ O(n)
         {
             int time = 1;
             int current = 0;
 
-            for (int j = i; j < n; j++) 
+            for (int j = i; j < n; j++) // Nested loop → O(n^2)
             {
-                current += satisfaction[j] * time;
+                current += satisfaction[j] * time; //→ O(1)
                 time++;
             }
 
-            maxSat = max(maxSat, current);
+            maxSat = max(maxSat, current);//→ O(1)
         }
 
         return maxSat;
